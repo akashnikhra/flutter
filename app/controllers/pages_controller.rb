b71f88bbc6ca90b3bce5e694f5d6364b579d289b
Akash Nikhra <akashnikhra@hotmail.com>
2016-05-04 05:49:10 +0000

    Adding username column to database and to other pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,10 +11,15 @@ class PagesController < ApplicationController
 
   #back-end code for pages/profile
   def profile
+    # grab username from the URL as :id
+    if(User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      redirect_to root_path, :notice => "User Not Found!"
+    end
   end
 
   #back-end code for pages/explore
   def explore
   end
-  
 end
